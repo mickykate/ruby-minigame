8faaeae738b637e8c496980b5f7c501f5c9a22d6
mickykate <166803913+mickykate@users.noreply.github.com>
2024-05-30 03:43:15 +0900

    Update game.rb

--- game.rb
+++ game.rb
@@ -1,12 +1,13 @@
-puts "最初はグーじゃんけん・・・・"
+puts "最初はグーじゃんけん・・・・" #あいこはじゃんけん繰り返し、勝ちまたは負けはあっち向いてホイに進むようにする
 class MiniGame
+  $case_pattern = nil
   def initialize(draw = "あいこ", lose ="負け", win ="勝ち")
     @draw = draw
     @lose = lose
     @win = win
   end
-  
-def janken  #じゃんけんの実装
+
+def janken #じゃんけんの実装
   puts "[0]グー\n[1]チョキ\n[2]パー\n[3]戦わない"
   
   player_hand = gets.to_i
@@ -26,23 +27,22 @@ end
 
   jankens = ["グー", "チョキ","パー","戦わない"]
   puts "あなたは#{jankens[player_hand]}を出しました\n相手は#{jankens[program_hand]}を出しました"
+  puts "---------------"
 
-  if player_hand == program_hand
+  
+  if(player_hand == program_hand)
     puts "#{@draw}で..."
-    puts "ショ！"
     return true
   elsif(player_hand == 0 && program_hand == 1)|| (player_hand == 1 && program_hand == 2)|| (player_hand == 2 && program_hand == 0)
-    puts "あなたは#{@win}ました"
-    return false #attimuite_hoiメソッドを呼び出す
+    return false #attimuite_hoiメソッドを呼び出す 2回目はなぜかattimuite_hoiにいこうできない。確認中
     attimuite_hoi
-  else (player_hand == 0 && program_hand == 2)|| (player_hand == 1 && program_hand == 0)|| (player_hand == 2 && program_hand == 1)
-    puts "あなたは#{@lose}ました"
-    return false #attimuite_hoiメソッドを呼びだす
+  else(player_hand == 0 && program_hand == 2)|| (player_hand == 1 && program_hand == 0)|| (player_hand == 2 && program_hand == 1)
+    return false #attimuite_hoiメソッドを呼びだす　　2回目はなぜかattimuite_hoiにいこうできない。確認中
     attimuite_hoi
   end
 end
 
-#あいこはじゃんけん繰り返し、勝ちまたは負けはあっち向いてホイに進むようにする
+
 def attimuite_hoi #あっち向いてホイの処理
   puts "あっち向いて・・・"
   puts "[0]上\n[1]下\n[2]右\n[3]左"
@@ -55,17 +55,17 @@ def attimuite_hoi #あっち向いてホイの処理
   attimuite_hoi = ["上", "下", "右", "左"]
   puts "あなた:#{attimuite_hoi[player_direction]}, あいて:#{attimuite_hoi[program_direction]}"
 
-  if(player_direction == program_direction)
-    puts "あなたの#{@win}です"
-    return false
-  elsif
-    puts "あなたの#{@lose}です"
-    return false
+  if player_direction == program_direction #試行錯誤中　
+     puts "あなたの#{@win}です"　　　　　　　　　　　#じゃんけん：勝ち、あっち向いてホイ：同じ方向を向く→勝ち　　じゃんけん：負け、あっち向いてホイ：同じ方向を向く→負け　としたい。
+     puts "---------------"
+     return false
+     exit
   else
-    puts "#{@draw}です。もう一度..."
-    return true
+    puts "引き分け。ジャンケンを再開"
+    puts "---------------"
+    puts "じゃんけん..."
+    MiniGame.new.janken
   end
- end
 end
 
 next_game = true
@@ -74,8 +74,6 @@ while next_game
   next_game = MiniGame.new.janken
 end
 
-next2_game = true
-
-while next2_game
-  next2_game = MiniGame.new.attimuite_hoi
+MiniGame.new.attimuite_hoi
 end
+
